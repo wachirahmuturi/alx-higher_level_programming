65909f387a11b73460daa3c645c1a1809f12029a
wachirahmuturi <wachiramuturi18@gmail.com>
2023-01-09 09:44:22 +0000

    use an alternative method

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,5 +2,7 @@
 def new_in_list(my_list, idx, element):
     list_copy = my_list[:]
     if idx < 0 or idx >= len(my_list):
-        list_copy[idx] = element
-    return list_copy
+        return list_copy
+    else:
+	list_copy[idx] = element
+        return list_copy
