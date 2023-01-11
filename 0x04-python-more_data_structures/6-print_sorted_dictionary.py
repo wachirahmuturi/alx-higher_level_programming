3f90d540a1638d80b626c7d8dbe721aa517f95b3
wachirahmuturi <wachiramuturi18@gmail.com>
2023-01-11 10:17:25 +0000

    Fix indentation error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,3 @@ def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None:
         for key, value in sorted(a_dictionary.items()):
             print("{}: {}".format(key, value))
-    return()
